a52b1848019d20c8566702f5ba3e21bc2967e2f4
rokhssi oumayma <rokhssioumayma@gmail.com>
2026-01-06 18:02:10 +0100

    hebergement

--- app.py
+++ app.py
@@ -272,5 +272,6 @@ def translate_audio(n_clicks, contents, target_lang):
 # ======================
 # RUN
 # ======================
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8050))  
+    app.run_server(host="0.0.0.0", port=port)
